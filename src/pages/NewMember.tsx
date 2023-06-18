ab393f287a7fd4b9123604b006f4e639746be5fb
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-18 23:00:05 +0200

    newmember form styling

--- src/pages/NewMember.tsx
+++ src/pages/NewMember.tsx
@@ -6,29 +6,51 @@ function NewMember(){
             <form className="newMember-Form">
                 <div style={{width:'40%', flex:'1 1 40%'}}>
                     <label htmlFor="lastname">Lastname</label>
-                    <input name="lastname" type="text"/>
+                    <input id="lastname" type="text"/>
                     <label className='defaultSpacing' htmlFor="birthdate">Birthdate</label>
-                    <input name="birthdate" type="text"/>
+                    <input id="birthdate" type="text"/>
                 </div>
                 <div style={{width:'40%', flex:'1 1 40%'}}>
                     <label htmlFor="firstname">Firstname</label>
-                    <input name="firstname" type="text"/>
+                    <input id="firstname" type="text"/>
                     <label className='defaultSpacing' htmlFor="gender">Gender</label>
-                    <input name="gender" type="text"/>
+                    <input id="gender" type="text"/>
                 </div>
                 <div style={{width:'100%'}}>
                     <label className='defaultSpacing' htmlFor="address1">Address [1]</label>
-                    <input name="address1" type="text"/>
+                    <input id="address1" type="text"/>
                     <label className='defaultSpacing' htmlFor="address1">Address [2]</label>
-                    <input name="address1" type="text"/>
+                    <input id="address1" type="text"/>
                 </div>
                 <div style={{width:'40%', flex:'1 1 40%'}}>
                     <label className='defaultSpacing' htmlFor="postalcode">Postal Code</label>
-                    <input name="postalcode" type="text"/>
+                    <input id="postalcode" type="text"/>
                 </div>
                 <div style={{width:'40%', flex:'1 1 40%'}}>
                     <label className='defaultSpacing' htmlFor="city">City</label>
-                    <input name="city" type="text"/>
+                    <input id="city" type="text"/>
+                </div>
+
+                <div style={{width:'40%', flex:'1 1 40%'}}>
+                    <label className='defaultSpacing' htmlFor="phone">Phone</label>
+                    <input id="phone" type="text"/>
+                    <label className='defaultSpacing' htmlFor="email">Email</label>
+                    <input id="email" type="text"/>
+                </div>
+                <div style={{width:'40%', flex:'1 1 40%'}}>
+                    <label className='defaultSpacing' htmlFor="mobile">Mobile</label>
+                    <input id="mobile" type="text"/>
+                </div>
+
+                <div style={{width:'40%', flex:'1 1 40%'}}>
+                    <label className='defaultSpacing' htmlFor="emergencyContact-lastname">Lastname</label>
+                    <input id="emergencyContact-lastname" type="text"/>
+                    <label className='defaultSpacing' htmlFor="emergencyContact-mobile">Mobile</label>
+                    <input id="emergencyContact-mobile" type="text"/>
+                </div>
+                <div style={{width:'40%', flex:'1 1 40%'}}>
+                    <label htmlFor="emergencyContact-firstname">Firstname</label>
+                    <input id="emergencyContact-firstname" type="text"/>
                 </div>
                 
             </form>
