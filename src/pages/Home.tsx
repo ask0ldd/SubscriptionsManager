cf9a9cb38e60aa327dc5c2846185501d089f8bfa
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-18 15:57:17 +0200

    customrouter base

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import '../App.css'
+
 function Home() {
   
     return (
-        <>
-        </>
+        <form>
+        <label>Login</label>
+        <input type="text"/>
+        <label className='defaultSpacing'>Password</label>
+        <input type="text"/>
+        <input type="submit"/>
+      </form>
     )
   }
   
