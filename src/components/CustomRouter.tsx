7eeec929f5cd54e61eada45148bbb62551e6bbba
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-19 15:29:51 +0200

    newmember form styling

--- src/components/CustomRouter.tsx
+++ src/components/CustomRouter.tsx
@@ -8,7 +8,7 @@ function CustomRouter() {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/newMember" element={<NewMember />} />
+                <Route path="/newmember" element={<NewMember />} />
             </Routes>
         </Router>
     )
