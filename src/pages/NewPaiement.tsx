ea397ebe1430b705bc88d30c6059ab3f7df2fa25
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-19 22:16:39 +0200

    newpaiement more inputs

--- src/pages/NewPaiement.tsx
+++ src/pages/NewPaiement.tsx
@@ -4,20 +4,34 @@ function NewPaiement(){
     return (
         <main>
             <form className="newPaiement-Form">
-                <div style={{display:'flex'}}>
-                    <div>
+                <div className='trioletContainer'>
+                    <div className='triolet'>
                         <label>Activity</label>
                         <input type='text'/>
                     </div>
-                    <div>
-                        <label>Subscription Duration</label>
-                        <input type='text'/>
-                    </div>
-                    <div>
+                    <div className='triolet'>
                         <label>Starting Date</label>
                         <input type='text'/>
                     </div>
+                    <div className='triolet'>
+                        <label>Subscription Duration</label>
+                        <input type='text'/>
+                    </div>
+                </div>
 
+                <div className='trioletContainer'>
+                    <div className='triolet'>
+                        <label className='defaultSpacing'>Registration Fees</label>
+                        <input type='text'/>
+                    </div>
+                    <div className='triolet'>
+                        <label className='defaultSpacing'>Payment Method</label>
+                        <input type='text'/>
+                    </div>
+                    <div className='triolet'>
+                        <label className='defaultSpacing'>Payment Date</label>
+                        <input type='text'/>
+                    </div>
                 </div>
 
             </form>
