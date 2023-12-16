bb00791799517c68625a649bbbc761faa837a690
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-16 21:00:25 +0100

    validator update

--- src/services/validator.tsx
+++ src/services/validator.tsx
@@ -12,16 +12,16 @@ export class Validators {
         return numberRegex.test(trimmedValue)
     }
 
-    static isNumber(inputValue : string) : boolean{
+    /*static isNumber(inputValue : string) : boolean{
         return !Number.isNaN(Number.parseInt(inputValue))
     }
 
     static isPositiveNumber(inputValue : string) : boolean{
         return !Number.isNaN(Number.parseInt(inputValue)) && Number.parseInt(inputValue) > 0
-    }
+    }*/
 
     // !!! past date & future date needed
-    static isDate(inputValue : string) : boolean{
+    /*static isDate(inputValue : string) : boolean{
         const trimmedValue = inputValue.trim()
         const dateRegexDash = new RegExp("^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$")
         const dateRegexSlash = new RegExp("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$")
@@ -33,6 +33,23 @@ export class Validators {
             isPastDate = (Date.parse(formattedDate) - Date.now()) < 0
         }
         return (dateRegexDash.test(trimmedValue) || dateRegexSlash.test(trimmedValue)) && isPastDate
+    }*/
+
+    static isDate(value: string) : boolean {
+        const USFormatRegex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/
+        const FRFormatRegex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/
+        return USFormatRegex.test(value.trim()) || FRFormatRegex.test(value.trim())
+    }
+
+    static isNumber(value: string) : boolean {
+        const numberRegex = /^[0-9]+$/
+        return numberRegex.test(value.trim())
+    }
+
+    static isDatePast(date: string) : boolean{
+        if(!Validators.isDate(date)) return false
+        console.log(new Date(date) < new Date())
+        return new Date(date) < new Date()
     }
 
     static isEmail(inputValue : string) : boolean{
